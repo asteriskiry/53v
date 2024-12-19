1032a2cd444578b108d43184f8e6be86487709ae
allurei <suhonen.laakso@gmail.com>
2024-12-19 17:01:13 +0200

    test

--- src/routes/tesmi/+page.server.js
+++ src/routes/tesmi/+page.server.js
@@ -1,5 +1,5 @@
-/* export const load = async ({fetch, params }) => {
-    const response = await fetch('https://www.asteriski.fi/wp-json/wp/v2/posts?tags=2200&_embed&per_page=4');
+export const load = async ({fetch, params }) => {
+    /* const response = await fetch('https://www.asteriski.fi/wp-json/wp/v2/posts?tags=2200&_embed&per_page=4');
 
     if (response.ok) {
         const data = await response.json();
@@ -7,9 +7,9 @@
         return {
             posts: data
         };
-    }
+    } */
 
     return {
         posts: []
     };
-} */
+}
