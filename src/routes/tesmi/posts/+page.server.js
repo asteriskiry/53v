4332476254ed40beec24dcffad53a882ee8239cc
allurei <suhonen.laakso@gmail.com>
2024-12-27 17:50:41 +0200

    does it work on client

--- src/routes/tesmi/posts/+page.server.js
+++ src/routes/tesmi/posts/+page.server.js
@@ -1,18 +1,16 @@
 export const load = async ({fetch, url }) => { 
     
-    const response = await fetch('https://www.asteriski.fi/wp-json/wp/v2/posts?tags=2200&_embed&per_page=4', {
-        headers: {
-            origin: url.origin
-        }
+    /* const response = await fetch('https://www.asteriski.fi/wp-json/wp/v2/posts?tags=2200&_embed&per_page=4', {
+        method: 'GET'
     });
 
     if (response.ok) {
         const data = await response.json();
-
+        console.log(response)
         return {
             posts: data
         };
-    }
+    } */
 
     return {
         posts: []
--- src/routes/tesmi/posts/+page.svelte
+++ src/routes/tesmi/posts/+page.svelte
@@ -2,6 +2,15 @@
     import Posts from "$lib/components/posts.svelte";
     export let data;
 
+    async function getPosts() {
+        const response = await fetch('https://www.asteriski.fi/wp-json/wp/v2/posts?tags=2200&_embed&per_page=4', {
+            method: 'GET'
+        });
+
+        const data = await response.json();
+        console.log(data);
+    }
 </script>
 
 <Posts posts={data.posts}/>
+<button on:click={getPosts}>nappula</button>
